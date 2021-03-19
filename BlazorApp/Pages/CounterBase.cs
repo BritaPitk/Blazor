fe0fe75f06f01e4a7eed3e5c7ab275b42796d71b
LAPTOP-A53F6E9E\brita <brita.pitk@gmail.com>
2021-03-19 14:01:15 +0200

    Video nr 6: tegime Employee Magement Webi

--- BlazorApp/Pages/CounterBase.cs
+++ BlazorApp/Pages/CounterBase.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlazorApp.Pages
 {
-    public class CounterBase
+    public class CounterBase : ComponentBase
     {
+        protected int currentCount = 0;
+        protected string fontFamily = "Verdana";
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
     }
 }
