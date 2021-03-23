d353a9aa31b983241f09dbc2a9d91aa17c9e1280
LAPTOP-A53F6E9E\brita <brita.pitk@gmail.com>
2021-03-23 08:54:41 +0200

    Muudatused viimasest videost

--- EmployeeManagement.Api/Models/DepartmentRepository.cs
+++ EmployeeManagement.Api/Models/DepartmentRepository.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using EmployeeManagement.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace EmployeeManagement.Api.Models
 {
-    public class DepartmentRepository
+    public class DepartmentRepository : IDepartmentRepository
     {
+        private readonly AppDbContext appDbContext;
+
+        public DepartmentRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public Department GetDepartment(int departmentId)
+        {
+            return appDbContext.Departments
+                .FirstOrDefault(d => d.DepartmentId == departmentId);
+        }
+
+        public IEnumerable<Department> GetDepartments()
+        {
+            return appDbContext.Departments;
+        }
     }
 }
